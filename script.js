cbdd1fe71a1b9ac482d26bc2b3e75f8c535c4672
JakubPoznański <jqbpoz02@gmail.com>
2024-06-10 15:35:49 +0200

    Moving by Keyboard

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-console.error("works");
 const vertexShaderTxt = `
     precision mediump float;
 
-    attribute vec2 vertPosition;
+    uniform mat4 mWorld;
+    uniform mat4 mView;
+    uniform mat4 mProjection;
+    
+    attribute vec3 vertPosition;
     attribute vec3 vertColor;
 
     varying vec3 fragColor;
 
     void main() {
         fragColor = vertColor;
-        gl_Position = vec4(vertPosition, 0.0, 1.0);
+        gl_Position = mProjection * mView * mWorld * vec4(vertPosition, 1.0);
     }
 `;
 const fragmentShaderTxt = `
@@ -21,34 +24,20 @@ const fragmentShaderTxt = `
         gl_FragColor = vec4(fragColor, 1.0);
     }
 `;
+const mat4 = glMatrix.mat4;
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-let canvasColor = [0.2, 0.5, 0.8];
-let figureColor = [0.32, 1, 0.27];
-
-const button = document.querySelector(".button");
-const rgb = document.querySelector(".rgb");
-button.addEventListener("click", function () {
-  let R = getRandomNumber(0, 255);
-  let G = getRandomNumber(0, 255);
-  let B = getRandomNumber(0, 255);
-  rgb.textContent = `Current color : 
-  RGB (${R},${G},${B})`;
-  figureColor = [R / 255, G / 255, B / 255];
-  displayAll();
-});
-
-const first = function () {
-  const canvas = document.getElementById("first-canvas");
+const Triangle = function () {
+  const canvas = document.getElementById("main-canvas");
   const gl = canvas.getContext("webgl");
+  let canvasColor = [0.2, 0.5, 0.8];
 
   checkGl(gl);
 
-  gl.clearColor(...canvasColor, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clearColor(...canvasColor, 1.0); // R, G, B,  A
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.CULL_FACE);
 
   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -66,32 +55,78 @@ const first = function () {
   gl.attachShader(program, fragmentShader);
 
   gl.linkProgram(program);
+  checkLink(gl, program);
 
   gl.detachShader(program, vertexShader);
   gl.detachShader(program, fragmentShader);
 
   gl.validateProgram(program);
 
-  let triangleVerts = [
-    // X, Y
-    -0.8, -0.8, 0.0, -0.8, 0.8, 0, 0.8, -0.8, 0.0, 0.8, 0.8, 0.0, -0.8, 0.8, 0,
-    0.8, -0.8, 0.0,
+  const boxVertices = [
+    // X, Y, Z
+    // Top
+    -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
+
+    // Left
+    -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
+
+    // Right
+    1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0,
+
+    // Front
+    1.0, 1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0,
+
+    // Back
+
+    1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
+
+    // Bottom
+    -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0,
+  ];
+
+  const boxIndices = [
+    // Top
+    0, 1, 2, 0, 2, 3,
+
+    // Left
+    5, 4, 6, 6, 4, 7,
+
+    // Right
+    8, 9, 10, 8, 10, 11,
+
+    // Front
+    13, 12, 14, 15, 14, 12,
+
+    // Back
+    16, 17, 18, 16, 18, 19,
+
+    // Bottom
+    21, 20, 22, 22, 20, 23,
   ];
 
   let colors = [
-    ...figureColor,
-    ...figureColor,
-    ...figureColor,
-    ...figureColor,
-    ...figureColor,
-    ...figureColor,
+    1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.2, 1.0, 0.5, 1.0, 0.0,
+
+    0.8, 0.0, 0.2, 0.0, 1.0, 1.0, 0.0, 0.2, 1.0, 0.5, 1.0, 0.0,
+
+    1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.2, 1.0, 0.5, 1.0, 0.0,
+
+    1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.2, 1.0, 0.5, 1.0, 0.0,
+
+    1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.2, 1.0, 0.5, 1.0, 0.0,
+
+    1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.2, 1.0, 0.5, 1.0, 0.0,
   ];
 
-  const triangleVertBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertBuffer);
+  const boxVertBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, boxVertBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+
+  const boxIndicesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndicesBuffer);
   gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array(triangleVerts),
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(boxIndices),
     gl.STATIC_DRAW
   );
 
@@ -124,152 +159,74 @@ const first = function () {
   // render time
 
   gl.useProgram(program);
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
-};
 
-function checkGl(gl) {
-  if (!gl) {
-    console.log("WebGL not suppoerted, use another browser");
-  }
-}
+  const worldMatLoc = gl.getUniformLocation(program, "mWorld");
+  const viewMatLoc = gl.getUniformLocation(program, "mView");
+  const projectionMatLoc = gl.getUniformLocation(program, "mProjection");
 
-function checkShaderCompile(gl, shader) {
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error("shader not compiled", gl.getShaderInfoLog(shader));
-  }
-}
+  const worldMatrix = mat4.create();
+  const viewMatrix = mat4.create();
+  mat4.lookAt(viewMatrix, [0, 0, -4], [0, 0, 0], [0, 1, 0]);
+  const projectionMatrix = mat4.create();
+  mat4.perspective(
+    projectionMatrix,
+    glMatrix.glMatrix.toRadian(60),
+    canvas.width / canvas.height,
+    1,
+    10
+  );
 
-function checkLink(gl, program) {
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error("linking error", gl.getProgramInfoLog(program));
-  }
-}
+  gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
+  gl.uniformMatrix4fv(viewMatLoc, gl.FALSE, viewMatrix);
+  gl.uniformMatrix4fv(projectionMatLoc, gl.FALSE, projectionMatrix);
 
-const displayAll = function () {
-  first();
-  second();
-};
+  const identityMat = mat4.create();
+  let angle = 0;
 
-const second = function () {
-  {
-    const canvas = document.getElementById("second-canvas");
-    const gl = canvas.getContext("webgl");
+  window.addEventListener("keydown", function (event) {
+    const moveSpeed = 0.1;
+    switch (event.key) {
+      case "ArrowUp":
+        viewMatrix[13] += moveSpeed;
+        break;
+      case "ArrowDown":
+        viewMatrix[13] -= moveSpeed;
+        break;
+      case "ArrowLeft":
+        viewMatrix[12] -= moveSpeed;
+        break;
+      case "ArrowRight":
+        viewMatrix[12] += moveSpeed;
+        break;
+      case "w":
+        viewMatrix[14] -= moveSpeed;
+        break;
+      case "s":
+        viewMatrix[14] += moveSpeed;
+        break;
+    }
+    gl.uniformMatrix4fv(viewMatLoc, gl.FALSE, viewMatrix);
+  });
 
-    checkGl(gl);
+  const loop = function () {
+    angle = (performance.now() / 1000 / 10) * 2 * Math.PI;
+    mat4.rotate(worldMatrix, identityMat, angle, [1, 1, -0.5]);
+    gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
 
     gl.clearColor(...canvasColor, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 
-    gl.shaderSource(vertexShader, vertexShaderTxt);
-    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+    requestAnimationFrame(loop);
+  };
 
-    gl.compileShader(vertexShader);
-    gl.compileShader(fragmentShader);
-
-    checkShaderCompile(gl, vertexShader);
-
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-
-    gl.linkProgram(program);
-
-    gl.detachShader(program, vertexShader);
-    gl.detachShader(program, fragmentShader);
-
-    gl.validateProgram(program);
-
-    let triangleVerts = [
-      // X, Y
-      0.3, 0.8, 0.0, 0.8, 0.3, 0, 0, 0, 0, -0.3, 0.8, 0.0, -0.8, 0.3, 0, 0, 0,
-      0.0,
-
-      0.8, -0.3, 0.0, 0.3, -0.8, 0, 0, 0, 0.0, -0.8, -0.3, 0.0, -0.3, -0.8, 0,
-      0, 0, 0.0,
-
-      0.3, 0.8, 0.0, -0.3, 0.8, 0, 0, 0, 0,
-
-      -0.8, 0.3, 0.0, -0.8, -0.3, 0, 0, 0, 0.0,
-
-      0.8, 0.3, 0.0, 0.8, -0.3, 0, 0, 0, 0.0,
-
-      0.3, -0.8, 0.0, -0.3, -0.8, 0, 0, 0, 0,
-    ];
-
-    let colors = [
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-      ...figureColor,
-    ];
-
-    const triangleVertBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertBuffer);
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array(triangleVerts),
-      gl.STATIC_DRAW
-    );
-
-    const posAttribLocation = gl.getAttribLocation(program, "vertPosition");
-    gl.vertexAttribPointer(
-      posAttribLocation,
-      3,
-      gl.FLOAT,
-      gl.FALSE,
-      3 * Float32Array.BYTES_PER_ELEMENT,
-      0
-    );
-    gl.enableVertexAttribArray(posAttribLocation);
-
-    const triangleColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-    const colorAttribLocation = gl.getAttribLocation(program, "vertColor");
-    gl.vertexAttribPointer(
-      colorAttribLocation,
-      3,
-      gl.FLOAT,
-      gl.FALSE,
-      3 * Float32Array.BYTES_PER_ELEMENT,
-      0
-    );
-    gl.enableVertexAttribArray(colorAttribLocation);
-
-    // render time
-
-    gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLES, 0, 24);
-  }
+  requestAnimationFrame(loop);
 };
 
 function checkGl(gl) {
   if (!gl) {
-    console.log("WebGL not suppoerted, use another browser");
+    console.log("WebGL not supported, use another browser");
   }
 }
 
